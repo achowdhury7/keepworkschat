361f0c4f9e5ef6a526ef45b91d91b7b5ba1b3748
Abhijit <abhijit.chowdhury10192@gmail.com>
2016-12-08 15:21:03 +0530

    code style check

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express		 	 = require('express');
-var app 		 	 	 = express();
-var http 		 	 	 = require('http').createServer(app);
-var socket 		 	 = require('socket.io');
-var logger 		 	 = require('morgan');
-var path 		 	 	 = require('path');
-var mongoose 	 	 = require('mongoose');
-var config 		 	 = require('./config/config');
+var express      = require('express');
+var app          = express();
+var http         = require('http').createServer(app);
+var socket       = require('socket.io');
+var logger       = require('morgan');
+var path         = require('path');
+var mongoose     = require('mongoose');
+var config       = require('./config/config');
 var socketEvents = require('./socketEvents');
 
 mongoose.connect(config.database);
